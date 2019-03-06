315d17f9968d5c92248ed117502b9b3bbfc95d76
François MATHIEU <maps_red@yahoo.fr>
2019-03-06 16:10:47 +0100

    Add JWTRefreshToken

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
